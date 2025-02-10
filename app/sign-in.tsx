ac030b6f62f04b53f4e9336240719ca84f754610
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-02-10 14:32:05 +0100

    Create the welcome screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,59 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import {
+  Image,
+  ScrollView,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import images from '@/constants/images';
+import icons from '@/constants/icons';
 
 const SignIn = () => {
+  const handleLogin = () => {
+    // Implement Google login
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to Restate
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's get You Closer to {'\n'}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-center text-lg font-rubik text-black-200 mt-12">
+            Login to Restate with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-600 rounded-full w-full py-4 mt-5"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
